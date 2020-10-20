b98f2eb6839695a2e491a009ea96cb51279ddd90
zz132456zz <zz132456zz@kakao.com>
2020-10-20 17:33:47 +0900

    2020_10_20

---
+++ Java_Level1/Level1_budget.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Level1_budget {
+
+	public static void main(String[] args) {
+		int[] d = {1,3,2,5,4};
+		int budget = 9;
+		int answer = 0;
+		
+		Arrays.sort(d);
+		
+		for(int i=0;i<d.length;i++) {
+			budget -= d[i];
+			if(budget < 0) {
+				//return answer;
+			}
+			answer++;
+		}		
+		
+		for(int i=0;i<d.length;i++) {
+			System.out.println(d[i]);
+		}
+		
+		
+		System.out.println(answer);
+	}
+
+}
