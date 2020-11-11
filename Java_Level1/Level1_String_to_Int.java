f5405875b768067fcdf2e87f65cd58b63940211a
zz132456zz <zz132456zz@kakao.com>
2020-11-11 15:28:45 +0900

    2020_11_11

---
+++ Java_Level1/Level1_String_to_Int.java
@@ -0,0 +1,14 @@
+// 프로그래머스 - 문자열을 정수로 바꾸기
+
+public class Level1_String_to_Int {
+
+	public static void main(String[] args) {
+		String s = "-1234";
+		
+		int answer = Integer.parseInt(s);
+		
+		System.out.println(answer);
+
+	}
+
+}
